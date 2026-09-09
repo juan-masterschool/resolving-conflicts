747660fa3718144c46458ed37068424199e8b90d
Juan <bavaresco.juan@gmail.com>
2026-09-09 12:07:04 +0200

    new commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     name = input("What's your name?: ")
     hobby = input("What's your favorite hobby?: ")
+    city = input("Tell me the city you like the most: ")
 
 if __name__ == "__main__":
   main()
