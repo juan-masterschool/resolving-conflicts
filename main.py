c5034a4b0232a5a244b8e0b3fa04a3c1fbfdf664
Juan <bavaresco.juan@gmail.com>
2026-09-09 11:58:58 +0200

    first change on the hobby branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     name = input("What's your name?: ")
-    age = input("What's your age?: ")
+    hobby = input("What's your favorite hobby?: ")
 
 if __name__ == "__main__":
   main()
